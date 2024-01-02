70e095927961d7a55c47c2673dee233f991fbeba
encodeunlimited <118004281+encodeunlimited@users.noreply.github.com>
2024-01-02 09:34:00 +0530

    db_cleared

---
+++ database/cleardb.sql
@@ -0,0 +1,22 @@
+TRUNCATE TABLE discussion_list;
+ALTER TABLE discussion_list AUTO_INCREMENT = 1;
+
+TRUNCATE TABLE project_list;
+ALTER TABLE project_list AUTO_INCREMENT = 1;
+
+TRUNCATE TABLE task_list;
+ALTER TABLE task_list AUTO_INCREMENT = 1;
+
+TRUNCATE TABLE ticket_list;
+ALTER TABLE ticket_list AUTO_INCREMENT = 1;
+
+TRUNCATE TABLE discussion_list;
+ALTER TABLE discussion_list AUTO_INCREMENT = 1;
+
+TRUNCATE TABLE user_productivity;
+ALTER TABLE user_productivity AUTO_INCREMENT = 1;
+
+TRUNCATE TABLE users;
+ALTER TABLE users AUTO_INCREMENT = 1;
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES (NULL, 'Super', 'Admin', 'asd@gmail.com', 'b24331b1a138cde62aa1f679164fc62f', '1', 'male.jpg', current_timestamp());
